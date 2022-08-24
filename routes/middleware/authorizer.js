01131a17659d358c7f56ab82ce01f2e251d04d0a
abhijathbenhur <abhijath.benhur@tellius.com>
2022-08-24 23:56:35 +0530

    skipping token validation

--- routes/middleware/authorizer.js
+++ routes/middleware/authorizer.js
@@ -6,32 +6,33 @@ const authorizer = (req, res, next) => {
   // if (skipInjection.indexOf(req.path) == -1 && req.method != "OPTIONS") {
       const token = req.headers["x-access-token"];
       if (!token) {
-        console.log("Token not found!")
-        return res.status(403).json({
-          success: true,
-          data: {
-            error: "Authorization failed",
-          },
-        });
-      }
-      try {
-        const decoded = jwt.verify(token, process.env.TWEETER_KOO);
-        console.log("Decoded Token", decoded)
-        let key = decoded.key;
-        let secret = decoded.secret;
-        let tenantId = decoded.tenantId;
-        req.key = key;
-        req.secret = secret;
-        req.tenantId = tenantId;
-        return next();
-      } catch (err) {
-        console.log(err);
-        return res.status(401).json({
-          success: true,
-          data: {
-            error: "Authorization failed",
-          },
-        });
+        console.log("Token not found! All subsequent apis will fail!")
+        // return res.status(403).json({
+        //   success: true,
+        //   data: {
+        //     error: "Authorization failed",
+        //   },
+        // });
+      }else{
+        try {
+          const decoded = jwt.verify(token, process.env.TWEETER_KOO);
+          console.log("Decoded Token", decoded)
+          let key = decoded.key;
+          let secret = decoded.secret;
+          let tenantId = decoded.tenantId;
+          req.key = key;
+          req.secret = secret;
+          req.tenantId = tenantId;
+          return next();
+        } catch (err) {
+          console.log(err);
+          return res.status(401).json({
+            success: true,
+            data: {
+              error: "Authorization failed",
+            },
+          });
+        }
       }
     // } else {
     //   console.log("Skipped token validation")
